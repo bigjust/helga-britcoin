cd18280dd9fb478154f51772e9d5d595b5f4637c
Justin Caratzas <bigjust@lambdaphil.es>
2017-08-03 09:50:57 -0400

    change dev status to 'alpha'

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description=('HALP'),
     long_description=open('README.rst').read(),
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
